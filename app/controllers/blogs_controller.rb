db568cbe1c6f9719950dbe2fda8cb7d1c0e32e9d
wardou2 <ward92@gmail.com>
2019-10-03 23:00:43 -0700

    message status update, clean up comments

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,13 +2,11 @@ class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   # GET /blogs
-  # GET /blogs.json
   def index
     @blogs = Blog.all
   end
 
   # GET /blogs/1
-  # GET /blogs/1.json
   def show
   end
 
@@ -22,7 +20,6 @@ class BlogsController < ApplicationController
   end
 
   # POST /blogs
-  # POST /blogs.json
   def create
     @blog = Blog.new(blog_params)
 
@@ -36,7 +33,6 @@ class BlogsController < ApplicationController
   end
 
   # PATCH/PUT /blogs/1
-  # PATCH/PUT /blogs/1.json
   def update
     respond_to do |format|
       if @blog.update(blog_params)
@@ -48,12 +44,10 @@ class BlogsController < ApplicationController
   end
 
   # DELETE /blogs/1
-  # DELETE /blogs/1.json
   def destroy
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to blogs_url, notice: 'Blog was successfully deleted.' }
     end
   end
 
